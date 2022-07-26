8d1a457c8511b5c9cb6effda78f9a23b13b18a48
HENILCODES <prajapatihenil1112@gmail.com>
2022-07-26 08:06:59 +0530

    node

---
+++ json/h.js
@@ -0,0 +1,12 @@
+var a= 5;
+var a4 = "5";
+let a1 = 10;
+let a2 = "10";
+console.log(a);
+if (a1 === a2) {
+    console.log("equal");
+} else {
+    console.log("not equal");
+}
+let h;
+console.log(typeof a2);
