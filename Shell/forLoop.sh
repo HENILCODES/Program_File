daf693dc455e39021d76f8db40a353dc0e15b7e8
HENILCODES <prajapatihenil1112@gmail.com>
2022-09-09 08:17:03 +0530

    edit shell

---
+++ Shell/forLoop.sh
@@ -0,0 +1,24 @@
+echo  Enter number
+read n
+
+for (( i=0; i <= 2 i++ ))
+do 
+    echo "$i"
+done
+
+# for i in $(seq 1 2 20)
+# do
+#    echo "Welcome $i times"
+# done
+
+# PKGS="php7-openssl-7.3.19-r0  php7-common-7.3.19-r0  php7-fpm-7.3.19-r0  php7-opcache-7.3.19-r0 php7-7.3.19-r0"
+# for p in $PKGS
+# do
+#    echo "Installing $p package"
+# #    sudo apk add "$p"
+# done
+
+# for VAR in {2..20..2}
+# do
+#     echo $VAR
+# done
---
+++ Shell/tempCodeRunnerFile.sh
@@ -0,0 +1,4 @@
+(( i=0 ; i -le $n  ; i++ ))
+# do 
+#     echo $i
+# done
