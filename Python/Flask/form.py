ef0cdd2b7e1a2a22f2b572980c2a44485bf390b3
HENILCODES <prajapatihenil1112@gmail.com>
2022-09-24 13:02:16 +0530

    attech php admin to flask

--- Python/Flask/form.py
+++ Python/Flask/form.py
@@ -1,14 +1,31 @@
-from flask import *
-App = Flask(__name__)
-
-@App.route("/login")
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/')
+def form():
+    return render_template('login.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
 def login():
-    return render_template("login.html")
-
-@App.route("/log",methods=['post'])
-def log():
-    name = request.form['uname']
-    passs = request.form['pass']
-    return name + " | " + passs
-
-App.run(debug=True)
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO admin VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000,debug=True)
