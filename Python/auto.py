85d11cf08f6758a18441879c84a3eb4a4f5da9a4
HENILCODES <prajapatihenil1112@gmail.com>
2022-10-11 17:37:45 +0530

    edit some

--- Python/auto.py
+++ Python/auto.py
@@ -1,9 +1,11 @@
+import random
 import pyautogui as p
 import time
+val = ['How Are You Bro ?','I am Fine ','Hello','Hi','Welcome','What are you doing']
 i = 1
-while i <=5:
-    time.sleep(0)
-    p.typewrite("hello bol bol")
-    time.sleep(0)
+while True:
+    time.sleep(1)
+    p.typewrite(val[random.randint(0,4)])
+    time.sleep(1)
     p.press('enter')
-    i +=1
+
